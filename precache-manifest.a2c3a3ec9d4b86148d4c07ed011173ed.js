5efdd372a6ff23c53490ccb7b9625b3b0e00e3d1
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-06-13 08:05:18 -0600

    Updates

---
+++ precache-manifest.a2c3a3ec9d4b86148d4c07ed011173ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad06cbcd84f0264d8b3e42c30b30fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b77dc09d3402714c76",
+    "url": "/static/css/main.e4be32d3.chunk.css"
+  },
+  {
+    "revision": "99ca2e47d11420c0c5fb",
+    "url": "/static/js/2.39624fc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.39624fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b77dc09d3402714c76",
+    "url": "/static/js/main.eb66adb3.chunk.js"
+  },
+  {
+    "revision": "11be49072d2f076fb163",
+    "url": "/static/js/runtime-main.c36f934b.js"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/static/media/illustration-working.5029d049.svg"
+  }
+]);

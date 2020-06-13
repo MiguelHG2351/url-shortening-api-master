d13e7402acfbc4ff2ee691fabd387343545567cb
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-06-13 08:44:04 -0600

    Updates

--- precache-manifest.5c638c21adc2d4e61bd988a2ea94445b.js
+++ precache-manifest.5c638c21adc2d4e61bd988a2ea94445b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bbb5c2e24159b953e6b542cff033bb1b",
+    "revision": "416e320fbf6739c73729d87a2a79550d",
     "url": "/index.html"
   },
   {
-    "revision": "40b82fb524dfb8646f14",
-    "url": "/static/css/main.e4be32d3.chunk.css"
+    "revision": "3e30641655be29fcf00a",
+    "url": "/static/css/main.752f090b.chunk.css"
   },
   {
-    "revision": "99ca2e47d11420c0c5fb",
-    "url": "/static/js/2.39624fc2.chunk.js"
+    "revision": "a0144599dfca2d325edc",
+    "url": "/static/js/2.21311c10.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.39624fc2.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.21311c10.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "40b82fb524dfb8646f14",
-    "url": "/static/js/main.13eb0607.chunk.js"
+    "revision": "3e30641655be29fcf00a",
+    "url": "/static/js/main.d65f2efc.chunk.js"
   },
   {
     "revision": "11be49072d2f076fb163",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dfa0f2afd5fc904c250135e152d79c9.js"
+  "/precache-manifest.5c638c21adc2d4e61bd988a2ea94445b.js"
 );
 
 self.addEventListener('message', (event) => {

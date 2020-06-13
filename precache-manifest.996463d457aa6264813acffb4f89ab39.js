fbc3bec99e680c31ea6d11b69781d9c2f4fd8279
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-06-13 10:20:16 -0600

    Updates

---
+++ precache-manifest.996463d457aa6264813acffb4f89ab39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9208fa037ad9c05bb93ccaa1bdbbc9b1",
+    "url": "/url-shortening-api-master/index.html"
+  },
+  {
+    "revision": "ae059fecbc4c63c2bd76",
+    "url": "/url-shortening-api-master/static/css/main.5955355e.chunk.css"
+  },
+  {
+    "revision": "a0144599dfca2d325edc",
+    "url": "/url-shortening-api-master/static/js/2.21311c10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/url-shortening-api-master/static/js/2.21311c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae059fecbc4c63c2bd76",
+    "url": "/url-shortening-api-master/static/js/main.c755ab46.chunk.js"
+  },
+  {
+    "revision": "b1b374b14a5e3203aa0d",
+    "url": "/url-shortening-api-master/static/js/runtime-main.658a759f.js"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/url-shortening-api-master/static/media/illustration-working.5029d049.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c638c21adc2d4e61bd988a2ea94445b.js"
+  "/url-shortening-api-master/precache-manifest.996463d457aa6264813acffb4f89ab39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/url-shortening-api-master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
